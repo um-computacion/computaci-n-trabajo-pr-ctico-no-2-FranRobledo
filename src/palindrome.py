f3da1135aa43cd2d9c24d65504226c5f11fac182
FranRobledo <f.robledo@alumno.um.edu.ar>
2025-04-15 18:30:11 -0300

    Implementación de la función is_palindrome

--- src/palindrome.py
+++ src/palindrome.py
@@ -2,6 +2,20 @@ print ("Lector de palindromos - Consigna 2 Computacion - Francisco Robledo \n")
 
 
 class Palindromo ():
+    
+    def is_palindrome(palindrome):
+        palindrome = palindrome.lower()
+        palindrome = palindrome.replace(" ", "")
+        palindrome = palindrome.replace("à", "a")
+        palindrome = palindrome.replace("è", "e")
+        palindrome = palindrome.replace("ì", "i")
+        palindrome = palindrome.replace("ò", "o")
+        palindrome = palindrome.replace("ù", "u")
+        return True    
 
-    is_palindrome = str(input("Ingrese una palabra o frase: "))
-    print(is_palindrome)
+    palindrome = str(input("Ingrese una palabra o frase: "))
+    
+    if is_palindrome(palindrome):
+        print(palindrome + " " + "Es una palabra o frase palindroma")
+    else: 
+        print(palindrome + " " + "No es una palabra o frase palindroma")
